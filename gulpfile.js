7297c790a31f92209b41b24be23cc4ada6988d43
Yago Pérez Vázquez <yago.perezv@tecnocom.es>
2016-02-04 11:52:25 +0100

    Added tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,130 +1,133 @@
-var gulp = require("gulp"),
-    rimraf = require("rimraf"),
-    concat = require("gulp-concat"),
-    cssmin = require("gulp-cssmin"),
-    uglify = require("gulp-uglify"),
+var gulp = require('gulp'),
+    rimraf = require('rimraf'),
+    concat = require('gulp-concat'),
+    cssmin = require('gulp-cssmin'),
+    uglify = require('gulp-uglify'),
     path = require('path'),
     nodemon = require('gulp-nodemon');
 
 var paths = {
-    webroot: path.join(process.cwd(), "client")
+    webroot: path.join(process.cwd(), 'client')
 };
 
 
 paths.appJs = [
-    path.join(paths.webroot, "components/**/*.initialize.js"),
-    path.join(paths.webroot, "components/**/*.directive.js"),
-    path.join(paths.webroot, "components/**/*.factory.js"),
-    path.join(paths.webroot, "components/**/*.filter.js"),
-    path.join(paths.webroot, "components/**/*.controller.js"),
-    path.join(paths.webroot, "components/app.js")
+    path.join(paths.webroot, 'components/**/*.initialize.js'),
+    path.join(paths.webroot, 'components/**/*.directive.js'),
+    path.join(paths.webroot, 'components/**/*.factory.js'),
+    path.join(paths.webroot, 'components/**/*.filter.js'),
+    path.join(paths.webroot, 'components/**/*.controller.js'),
+    path.join(paths.webroot, 'components/app.js')
 ];
 
-paths.appCss = path.join(paths.webroot, "css/**/*.css");
+paths.appCss = path.join(paths.webroot, 'css/**/*.css');
 
 paths.vendorJs = [
-    path.join(paths.webroot, "lib/jquery/dist/jquery.js"),
-    path.join(paths.webroot, "lib/bootswatch-dist/js/bootstrap.js"),
-    path.join(paths.webroot, "lib/moment/moment.js"),
-    path.join(paths.webroot, "lib/angular/angular.js"),
-    path.join(paths.webroot, "lib/angular-messages/angular-messages.js"),
-    path.join(paths.webroot, "lib/angular-ui-router/release/angular-ui-router.js"),
-    path.join(paths.webroot, "lib/angular-bootstrap-datetimepicker/src/js/datetimepicker.js"),
-    path.join(paths.webroot, "lib/ng-dialog/js/ngDialog.js"),
-    path.join(paths.webroot, "lib/satellizer/satellizer.js"),
-    path.join(paths.webroot, "lib/angular-toastr/dist/angular-toastr.tpls.js")
+    path.join(paths.webroot, 'lib/jquery/dist/jquery.js'),
+    path.join(paths.webroot, 'lib/bootswatch-dist/js/bootstrap.js'),
+    path.join(paths.webroot, 'lib/moment/moment.js'),
+    path.join(paths.webroot, 'lib/angular/angular.js'),
+    path.join(paths.webroot, 'lib/angular-messages/angular-messages.js'),
+    path.join(paths.webroot, 'lib/angular-ui-router/release/angular-ui-router.js'),
+    path.join(paths.webroot, 'lib/angular-bootstrap-datetimepicker/src/js/datetimepicker.js'),
+    path.join(paths.webroot, 'lib/ng-dialog/js/ngDialog.js'),
+    path.join(paths.webroot, 'lib/satellizer/satellizer.js'),
+    path.join(paths.webroot, 'lib/angular-toastr/dist/angular-toastr.tpls.js')
 ];
 
 paths.vendorCss = [
-    path.join(paths.webroot, "lib/bootswatch-dist/css/bootstrap.css"),
-    path.join(paths.webroot, "lib/font-awesome/css/font-awesome.css"),
-    path.join(paths.webroot, "lib/angular-bootstrap-datetimepicker/src/css/datetimepicker.css"),
-    path.join(paths.webroot, "lib/ng-dialog/css/ngDialog.css"),
-    path.join(paths.webroot, "lib/ng-dialog/css/ngDialog-theme-default.css"),
-    path.join(paths.webroot, "lib/ng-dialog/css/ngDialog-theme-plain.css"),
-    path.join(paths.webroot, "lib/angular-toastr/dist/angular-toastr.css")
+    path.join(paths.webroot, 'lib/bootswatch-dist/css/bootstrap.css'),
+    path.join(paths.webroot, 'lib/font-awesome/css/font-awesome.css'),
+    path.join(paths.webroot, 'lib/angular-bootstrap-datetimepicker/src/css/datetimepicker.css'),
+    path.join(paths.webroot, 'lib/ng-dialog/css/ngDialog.css'),
+    path.join(paths.webroot, 'lib/ng-dialog/css/ngDialog-theme-default.css'),
+    path.join(paths.webroot, 'lib/ng-dialog/css/ngDialog-theme-plain.css'),
+    path.join(paths.webroot, 'lib/angular-toastr/dist/angular-toastr.css')
 ];
 
-paths.dist = path.join(paths.webroot, "dist/*");
-paths.concatAppJsDest = path.join(paths.webroot, "dist/app.js");
-paths.concatAppCssDest = path.join(paths.webroot, "dist/app.css");
-paths.concatVendorJsDest = path.join(paths.webroot, "dist/vendor.js");
-paths.concatVendorCssDest = path.join(paths.webroot, "dist/vendor.css");
+paths.dist = path.join(paths.webroot, 'dist/*');
+paths.concatAppJsDest = path.join(paths.webroot, 'dist/app.js');
+paths.concatAppCssDest = path.join(paths.webroot, 'dist/app.css');
+paths.concatVendorJsDest = path.join(paths.webroot, 'dist/vendor.js');
+paths.concatVendorCssDest = path.join(paths.webroot, 'dist/vendor.css');
 
 
 // Clean dist directory
-gulp.task("clean", function (cb) {
+gulp.task('clean', function (cb) {
     rimraf(paths.dist, cb);
 });
 
 // Minify js & css files
-gulp.task("min:js", function () {
-    gulp.src(paths.appJs, { base: "." })
+gulp.task('min:js', function () {
+    gulp.src(paths.appJs, { base: '.' })
         .pipe(concat(paths.concatAppJsDest))
         .pipe(uglify())
-        .pipe(gulp.dest("."));
+        .pipe(gulp.dest('.'));
 });
 
-gulp.task("min:vendorJsMin", function () {
-    gulp.src(paths.vendorJs, { base: "." })
+gulp.task('min:vendorJsMin', function () {
+    gulp.src(paths.vendorJs, { base: '.' })
         .pipe(concat(paths.concatVendorJsDest))
         .pipe(uglify())
-        .pipe(gulp.dest("."));
+        .pipe(gulp.dest('.'));
 });
 
-gulp.task("min:css", function () {
+gulp.task('min:css', function () {
     gulp.src([paths.appCss])
         .pipe(concat(paths.concatAppCssDest))
         .pipe(cssmin())
-        .pipe(gulp.dest("."));
+        .pipe(gulp.dest('.'));
 });
 
-gulp.task("min:vendorCssMin", function () {
+gulp.task('min:vendorCssMin', function () {
     gulp.src(paths.vendorCss)
         .pipe(concat(paths.concatVendorCssDest))
         .pipe(cssmin())
-        .pipe(gulp.dest("."));
+        .pipe(gulp.dest('.'));
 });
 
-gulp.task("min", ["min:js", "min:css", "min:vendorJsMin", "min:vendorCssMin"]);
+gulp.task('min', ['min:js', 'min:css', 'min:vendorJsMin', 'min:vendorCssMin']);
 
 // Concat js & css files for debugging
-gulp.task("concat:js", function () {
-    gulp.src(paths.appJs, { base: "." })
+gulp.task('concat:js', function () {
+    gulp.src(paths.appJs, { base: '.' })
         .pipe(concat(paths.concatAppJsDest))
-        .pipe(gulp.dest("."));
+        .pipe(gulp.dest('.'));
 });
 
-gulp.task("concat:vendorJsDebug", function () {
-    gulp.src(paths.vendorJs, { base: "." })
+gulp.task('concat:vendorJsDebug', function () {
+    gulp.src(paths.vendorJs, { base: '.' })
         .pipe(concat(paths.concatVendorJsDest))
-        .pipe(gulp.dest("."));
+        .pipe(gulp.dest('.'));
 });
 
-gulp.task("concat:css", function () {
+gulp.task('concat:css', function () {
     gulp.src([paths.appCss])
         .pipe(concat(paths.concatAppCssDest))
-        .pipe(gulp.dest("."));
+        .pipe(gulp.dest('.'));
 });
 
-gulp.task("concat:vendorCssDebug", function () {
+gulp.task('concat:vendorCssDebug', function () {
     gulp.src(paths.vendorCss)
         .pipe(concat(paths.concatVendorCssDest))
-        .pipe(gulp.dest("."));
+        .pipe(gulp.dest('.'));
 });
 
-gulp.task("concat", ["concat:js", "concat:css", "concat:vendorJsDebug", "concat:vendorCssDebug"]);
+gulp.task('concat', ['concat:js', 'concat:css', 'concat:vendorJsDebug', 'concat:vendorCssDebug']);
+
 
 // Watch for changes in the client app directory
-gulp.task("watch", function () {
+gulp.task('watch:client', function () {
     gulp.watch([paths.appJs, paths.appCss], ['concat']);
 });
 
 //Restart server on change
-gulp.task('backend', function() {
+gulp.task('watch:server', function() {
     nodemon({
-        script: 'server.js',
+        script: './server.js',
         ext: 'js',
         env: { 'NODE_ENV': 'development' }
     });
 });
+
+gulp.task('watch', ['concat', 'watch:client', 'watch:server']);
